bd6a701f52cbe8d7b9f892f6bd6740682401c667
MattMc <tooolbox@users.noreply.github.com>
2022-09-21 23:35:40 -0700

    Added the rest of the sql files to sort through and determine if we will make parse.

--- pkg/tsql/README.md
+++ pkg/tsql/README.md
@@ -1,4 +1,14 @@
-# Not Supported
+# Generation
+
+To regenerate the tsql parser, execute:
+
+```sh
+antlr2participle tsql.g4
+```
+
+The antlr2participle tool is experimental and lives in `internal`.
+
+## Not Supported
 
 - `ALTER DATABASE`
 - `PIVOT` and `UNPIVOT`
---
+++ pkg/tsql/possible-todo/ddl_procedures.sql
@@ -0,0 +1,33 @@
+create procedure a as begin
+	select 1
+end
+go
+create procedure b as begin
+	select 2
+end
+go
+
+-- call stored procedure, name in local var
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'TestSproc')
+  DROP PROCEDURE dbo.TestSproc
+GO
+
+CREATE PROCEDURE dbo.TestSproc @Name nvarchar(30) AS SELECT @Name RETURN
+GO
+
+DECLARE @SprocName nvarchar(64) = 'TestSproc';
+DECLARE @TestName nvarchar(64) = 'Foo'
+DECLARE @Result nvarchar(64)
+EXEC @Result = @SprocName @Name = @TestName;
+SELECT @Result
+GO
+
+
+CREATE OR ALTER PROC What_DB_is_this
+AS
+SELECT DB_NAME() AS ThisDB;
+GO
+
+CREATE PROCEDURE [DBO].[EXECUTEOLAP] @MY_PARAM NVARCHAR(MAX)
+WITH EXECUTE AS OWNER
+AS EXTERNAL NAME [MY_ASSEMBLY_NAME].[MY_NAMESPACE.MY_CLASS].[MY_METHOD]
