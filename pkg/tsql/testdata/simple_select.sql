effd5d48b82ef6d33ea62c2d182a987996c678c4
MattMc <tooolbox@users.noreply.github.com>
2022-09-22 16:57:41 -0700

    Return the simple_select.sql file

--- pkg/tsql/testdata/simple_select.sql
+++ pkg/tsql/testdata/simple_select.sql
@@ -1,25 +1,25 @@
 SELECT pp.LastName FROM Person.Person;
 
--- SELECT some_type::static_method (@arg1) FROM Product;
+SELECT some_type::static_method (@arg1) FROM Product;
 
--- SELECT still_one_type.non_static_method (@but, @with, @params) FROM Product;
+SELECT still_one_type.non_static_method (@but, @with, @params) FROM Product;
 
--- SELECT pp.LastName, pp.FirstName, e.JobTitle 
--- INTO dbo.EmployeeOne
--- FROM Person.Person AS pp JOIN HumanResources.Employee AS e
--- ON e.BusinessEntityID = pp.BusinessEntityID
--- WHERE LastName = 'Johnson';
+SELECT pp.LastName, pp.FirstName, e.JobTitle 
+INTO dbo.EmployeeOne
+FROM Person.Person AS pp JOIN HumanResources.Employee AS e
+ON e.BusinessEntityID = pp.BusinessEntityID
+WHERE LastName = 'Johnson';
 
--- SELECT p.Name AS ProductName, 
--- NonDiscountSales = (OrderQty * UnitPrice),
--- Discounts = ((OrderQty * UnitPrice) * UnitPriceDiscount)
--- FROM Production.Product AS p 
--- INNER JOIN Sales.SalesOrderDetail AS sod
--- ON p.ProductID = sod.ProductID 
--- ORDER BY ProductName DESC;
+SELECT p.Name AS ProductName, 
+NonDiscountSales = (OrderQty * UnitPrice),
+Discounts = ((OrderQty * UnitPrice) * UnitPriceDiscount)
+FROM Production.Product AS p 
+INNER JOIN Sales.SalesOrderDetail AS sod
+ON p.ProductID = sod.ProductID 
+ORDER BY ProductName DESC;
 
--- USE AdventureWorks2012;
--- GO
--- SELECT TOP 10.5 PERCENT *
--- FROM Production.Product
--- ORDER BY Name ASC;
+USE AdventureWorks2012;
+GO
+SELECT TOP 10.5 PERCENT *
+FROM Production.Product
+ORDER BY Name ASC;
